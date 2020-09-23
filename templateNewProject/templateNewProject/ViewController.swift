7a690a2230bb4b97b43e311b4746a97071635c2c
N Q Dat <32430754+nguyenquocdathcmus@users.noreply.github.com>
2020-09-23 18:25:56 +0900

    init project

---
+++ templateNewProject/templateNewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  templateNewProject
+//
+//  Created by nguyenquocdathcmus on 2020/09/23.
+//  Copyright © 2020 nguyenquocdat.hcmus@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
