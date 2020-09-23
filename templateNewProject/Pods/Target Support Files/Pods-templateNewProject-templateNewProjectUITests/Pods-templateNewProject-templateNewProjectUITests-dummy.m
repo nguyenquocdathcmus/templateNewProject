c5a6a3cae2dd2d0bef4208dd4c33a9c16671f696
N Q Dat <32430754+nguyenquocdathcmus@users.noreply.github.com>
2020-09-23 18:43:16 +0900

    init pod and install

---
+++ templateNewProject/Pods/Target Support Files/Pods-templateNewProject-templateNewProjectUITests/Pods-templateNewProject-templateNewProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_templateNewProject_templateNewProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_templateNewProject_templateNewProjectUITests
+@end
---
+++ templateNewProject/Pods/Target Support Files/Pods-templateNewProject/Pods-templateNewProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_templateNewProject : NSObject
+@end
+@implementation PodsDummy_Pods_templateNewProject
+@end
